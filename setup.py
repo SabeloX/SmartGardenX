d7939e41d2d56b51164acde162fac7b0a6114c89
SabeloX <sabelomtw@gmail.com>
2020-11-04 15:40:16 +0000

    ..

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/SabeloX/smartgarden_x',
-    version='0.5.0',
+    version='1.1.0',
     zip_safe=False,
 )
